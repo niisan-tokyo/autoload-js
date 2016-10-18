23a0abb91941ead016a7b96d4d655e0e4581dab6
新倉23 <r-niikura@c-tos.com>
2016-10-18 09:03:37 +0900

    name rule investigate

--- example/sample.js
+++ example/sample.js
@@ -1,11 +1,19 @@
 const rec = require('../index.js');
 
-rec(__dirname, 'app');
+rec.registerApps(__dirname, 'app');
+rec.registerModules(__dirname + '/../node_modules', 'mod')
 
 const file1 = new app.dir1.file1()
 file1.sayHello()
 
 const file2 = app.dir1.dir2.file2
 file2()
+
+const str = rec.nameRule('fs-readdir.-names')
+console.log(str)
+
+console.log(mod)
+const readdir = mod['fs-readdir-recursive']
+console.log(readdir('dir1'))
 
 console.log(app)
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const recursive = require('fs-readdir-recursive');
+const fs = require('fs')
 
-let namespace = {_pathList: {}}
+let namespace = {_pathList: {}, _modList: {}}
 
 function filter(name) {
     return name.slice(-3) === '.js'
 }
 
-function setName(name) {
+function modFilter(name, dirname) {
+    path = dirname + '/' + name
+    return fs.statSync(path).isDirectory() && name.slice(0, 1) != '.'
+}
+
+function setName(name, prefix) {
     arr = name.split('/')
-    temp = namespace
+    namespace[prefix] = namespace[prefix] || {}
+    temp = namespace[prefix]
     length = arr.length
     for (i = 0; i < length; i++) {
         if (i != length - 1) {
@@ -31,12 +38,35 @@ function setLoader(nameobj, filename, path) {
     }})
 }
 
-const autoloader = (dirname, appName) => {
-    paths = recursive(dirname)
-    namespace._basePath = dirname + '/'
-    mods = paths.filter(filter);
-    mods.forEach(setName)
-    global[appName] = namespace;
+function setModule(obj, name) {
+    Object.defineProperty(obj, name, {get: () => {
+        if (typeof namespace._modList[name] == 'undefined') {
+            namespace._modList[name] = require(name)
+        }
+
+        return namespace._modList[name]
+    }})
+}
+
+const autoloader = {
+    registerApps(dirname, appName) {
+        paths = recursive(dirname)
+        namespace._basePath = dirname + '/'
+        mods = paths.filter(filter);
+        mods.forEach(e => setName(e, appName))
+        global[appName] = namespace[appName];
+    },
+
+    registerModules(dirname, modName) {
+        mods = fs.readdirSync(dirname).filter(e => modFilter(e, dirname))
+        namespace[modName] = namespace[modName] || {}
+        mods.forEach(e => setModule(namespace[modName], e))
+        global[modName] = namespace[modName]
+    },
+
+    nameRule(name) {
+        return name.replace(/[._\-]+(.)/g, '$1'.toUpperCase())
+    }
 }
 
 module.exports = autoloader
